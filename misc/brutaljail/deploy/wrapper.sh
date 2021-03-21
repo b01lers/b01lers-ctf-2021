509a1eeec946ee8b688ccd46431183d29ad392dd
Nathan <nbanmp@gmail.com>
2021-03-21 14:17:18 -0700

    Brutaljail

---
+++ misc/brutaljail/deploy/wrapper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/brutaljail/ && python3 ./brutaljail.py
---
+++ misc/brutaljail/dist/brutaljail.py
@@ -0,0 +1,9 @@
+import sys
+line = sys.stdin.buffer.readline()
+banned = b".()=+-/"
+for char in banned:
+    if char in line:
+        print("Nope")
+        break
+else:
+    print(eval(line, {'globals': {}, '__builtins__': {'getattr': getattr}}))
